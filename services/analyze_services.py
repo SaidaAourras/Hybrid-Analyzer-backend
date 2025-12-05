9677bb5731853c64ff4f9b38e21ac4983371ad2e
SaidaAourras <sa123aouras@gmail.com>
2025-12-05 10:37:03 +0100

    [HA-17] : create functions create prompt and analyse with gemini

--- services/analyze_services.py
+++ services/analyze_services.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from google import genai    
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -43,7 +44,7 @@ def classifier_zero_shot(text , labels):
                 "parameters": {"candidate_labels": labels},
             })
         
-        return output[0]
+        return output[0] 
     
     # si le text est vide
     except ValueError as ve:
@@ -56,3 +57,51 @@ def classifier_zero_shot(text , labels):
     # erreur inattendue
     except Exception as e :
         return f'erreur {e}'
+
+
+
+API_KEY_GEMINI = os.getenv('API_KEY_GEMINI')
+# create prompt
+
+def create_prompt(text , labels):
+    input_data = classifier_zero_shot(text , labels)
+    score = input_data['score']
+    label = input_data['label']
+    
+    prompt = f"""
+                Tu es un modèle NLP spécialisé dans l'analyse et le résumé de textes.
+                Le texte fourni appartient à la catégorie : {label}.
+                
+                Tâches :
+                1. Produire un résumé clair, neutre et concis.
+                2. Identifier le ton général du texte (positif / neutre / négatif).
+                3. Retourner la réponse sous forme de JSON valide avec deux champs : "resume" et "ton".
+
+                Texte à analyser :
+                {text}
+
+                Réponds uniquement en JSON.
+                """
+    return prompt ,score
+
+
+# text = "La ville a inauguré aujourd’hui une nouvelle ligne de tramway destinée à améliorer les déplacements des habitants.Le projet, commencé il y a trois ans, a coûté près de 400 millions d’euros et fait partie d’un plan plus large visant à développer les transports publics et réduire l’usage des voitures."
+
+
+# prompt , score = create_prompt(text , labels)
+
+# synthèse contextuelle Gemini
+
+def analyse_with_gemini(prompt):
+    try:
+        client = genai.Client(api_key=API_KEY_GEMINI)
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt
+        )
+        return response.text
+    except Exception as e:
+        return f'erreur : {e}'
+    
+# analyse_with_gemini(prompt)
